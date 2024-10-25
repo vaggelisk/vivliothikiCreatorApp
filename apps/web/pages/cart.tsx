0a26c9dd73286bb9c94e342809695d2cf27c0416
vaggelisk <you@example.com>
2024-10-25 19:11:47 +0300

    feat: jfkdjfk

--- apps/web/pages/cart.tsx
+++ apps/web/pages/cart.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'next-i18next';
-import { CartPageContent } from '~/components';
+import {CartPageContent, Search} from '~/components';
 import { createGetServerSideProps } from '~/helpers';
 import { CheckoutLayout } from '~/layouts';
 
@@ -11,8 +11,14 @@ export function CartPage() {
   const { t } = useTranslation('cart');
 
   return (
-    <CheckoutLayout backLabel={t('backToShopping')} backHref="/category" heading={t('myCart')}>
-      <CartPageContent />
+    <CheckoutLayout backLabel={t('back to the Base')} backHref="/" heading={'Ουπς'}>
+      <div className="">
+        <div className="font-bold typography-headline-4 md:typography-headline-3">
+          Δεν υπάρχει βιβλίο που να ταιριάζει σε αυτό το isbn
+        </div>
+        <div>Τα παραπονά σας στο γαμω-κράτος και τους εκδότες, που μόνο για τα φράγκα νοιάζονται</div>
+
+      </div>
     </CheckoutLayout>
   );
 }
